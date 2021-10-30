541d23be850c0719e99c7dc31fd090c699b80234
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-10-30 13:54:36 +0300

    Added fetchUsers action creator

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { fetchUsers } from './store/reducers/ActionCreators';
 
 function App() {
+  const dispatch = useAppDispatch();
+  const { users, isLoading, error } = useAppSelector(state => state.userReducer);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
   return (
     <div className="App">
-      hello
+      {isLoading && <h1>Идет загрузка...</h1>}
+      {error && <h1>{error}</h1>}
+      {JSON.stringify(users, null, 2)}
     </div>
   );
 }
--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -17,7 +17,20 @@ const initialState: UserState = {
 export const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {},
+  reducers: {
+    usersFetching(state) {
+      state.isLoading = true;
+    },
+    usersFetchingSuccess(state, action: PayloadAction<IUser[]>) {
+      state.isLoading = false;
+      state.error = '';
+      state.users = action.payload;
+    },
+    usersFetchingError(state, action: PayloadAction<string>) {
+      state.isLoading = false;
+      state.error = action.payload;
+    }
+  },
   extraReducers: {}
 });
 
